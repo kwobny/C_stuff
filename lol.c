26cf7faae0211e0781474de04f159c43cce3b7e1
Yeongjin Kwon <yeongjinkwon@gmail.com>
2022-03-06 02:07:39 -0500

    Start implementing stuff

--- lol.c
+++ lol.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <search.h>
+
+const char letters[] = "abcdefghijklmnopqrstuvwxyz";
+const char digits[] = "0123456789";
+size_t numberOfLetters = sizeof(letters)-1;
+size_t numberOfDigits = sizeof(digits)-1;
+
+int compareChar(const char *key, const char *other) {
+    return *key - *other;
+}
 
 int main(void) {
-    printf("hello world\n");
+    for (char i = getchar(); i != EOF; i = getchar()) {
+        char *location = lfind(&i, letters, &numberOfLetters, sizeof(char), compareChar);
+        if (location == NULL) {
+            location = lfind(&i, digits, &numberOfDigits, sizeof(char), compareChar);
+        }
+
+hello:
+        // If character is a letter or digit
+        if (location != NULL)
+            location += 1;
+    }
     return 0;
 }
